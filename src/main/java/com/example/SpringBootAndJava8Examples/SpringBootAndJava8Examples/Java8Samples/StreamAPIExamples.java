0cfb0b11282e5ab752f1ca76967b04884664e64c
sudhakar0468 <pogula.sudhakarreddy@gmail.com>
2024-06-28 10:17:11 +0530

    import statements are missing

--- src/main/java/com/example/SpringBootAndJava8Examples/SpringBootAndJava8Examples/Java8Samples/StreamAPIExamples.java
+++ src/main/java/com/example/SpringBootAndJava8Examples/SpringBootAndJava8Examples/Java8Samples/StreamAPIExamples.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
